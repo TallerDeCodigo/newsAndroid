bbc3bcb07936ebc6328e4ed85a0a41e2bdd1fe16
hever <hever@tallerdecodigo.com>
2016-11-16 13:05:20 -0600

    fixed open app background

--- app/src/main/java/com/lisa/televisa/Splash2.java
+++ app/src/main/java/com/lisa/televisa/Splash2.java
@@ -23,9 +23,9 @@ public class Splash2 extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
 
         if (extras != null) {
-            Log.d(TAG, extras.toString());
-            Log.d(TAG, extras.getString("postID"));
-            postID = extras.getString("postID");
+
+            if(extras.getString("postID") != null)
+                postID = extras.getString("postID");
         }
 
         setContentView(R.layout.activity_splash2);
